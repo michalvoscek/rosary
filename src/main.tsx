6e4eda6fbc512b49615cd5f6ec9f22c25c86a57f
Michal Voscek <michal.voscek@gmail.com>
2026-06-13 12:53:17 +0200

    feat: reload page on service worker update

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.addEventListener("controllerchange", () => {
+    window.location.reload();
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename="/rosary/">
